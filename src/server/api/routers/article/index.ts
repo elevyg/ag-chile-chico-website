27b6aeb5f9db534800d167eda81aeadfc1de283e
elevyg <elevyg91@gmail.com>
2023-10-04 12:09:29 -0300

    fix: organization

--- src/server/api/routers/article/index.ts
+++ src/server/api/routers/article/index.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
-import { getArticle } from "~/server/api/procedures/getArticle";
-import { upsertArticle } from "~/server/api/procedures/upsertArticle";
+import { getArticle } from "~/server/api/routers/article/getArticle";
+import { upsertArticle } from "~/server/api/routers/article/upsertArticle";
 
 import { createTRPCRouter } from "~/server/api/trpc";
 
