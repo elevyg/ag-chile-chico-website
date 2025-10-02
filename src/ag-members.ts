6cae8422a7af8b230e351dfd70034a599706923f
elevyg <elevyg91@gmail.com>
2025-10-02 10:05:40 -0300

    feat: update ag members

--- src/ag-members.ts
+++ src/ag-members.ts
@@ -1,18 +1,3 @@
-// export const agMembers = {
-//   "h-de-la-patagonia": { placeId: "ChIJY-YrKz4r7b0RsqE46gSZzIc" },
-//   "brisas-del-lago": { placeId: "ChIJaaoRokYr7b0REjgJTQrZ7-M" },
-//   "costanera-apart": { placeId: "ChIJ9Qzx86Qr7b0R0dYTCLoQJUQ" },
-//   "cabañas-el-engaño": { placeId: "ChIJifhlQmwr7b0R9HSGzXZcklA" },
-//   "cabañas-frau-schuster": { placeId: "ChIJrZq5k0gr7b0Rk4ZHq8cqMkk" },
-//   patagoniaxpress: { placeId: "ChIJKc8480Yr7b0R_Y1oWU00opk" },
-//   "garnika-bike": { placeId: "ChIJFTC580Ar7b0Rru2WdKEOoU8" },
-//   "chile-chico-explora": { placeId: "ChIJbVP2IEQr7b0Rqz8LOgUMdf8" },
-//   "turismo-tramal": {
-//     placeId:
-//       "EjFNYW51ZWwgUm9kcsOtZ3VleiA0NDMsIENoaWxlIENoaWNvLCBBeXPDqW4sIENoaWxlIjESLwoUChIJt5eqmUYr7b0R4oYfNWy-yAoQuwMqFAoSCYsigc5FK-29EUFDVm7uin4E",
-//   },
-// };
-
 export const agMembers = {
   "h-de-la-patagonia": {
     name: "H de la Patagonia",
@@ -34,14 +19,6 @@ export const agMembers = {
     name: "Cabañas Frau Schuster",
     placeId: "ChIJrZq5k0gr7b0Rk4ZHq8cqMkk",
   },
-  patagoniaxpress: {
-    name: "Patagoniaxpress",
-    placeId: "ChIJKc8480Yr7b0R_Y1oWU00opk",
-  },
-  "garnika-bike": {
-    name: "Garnika Bike",
-    placeId: "ChIJFTC580Ar7b0Rru2WdKEOoU8",
-  },
   "chile-chico-explora": {
     name: "Chile Chico Explora",
     placeId: "ChIJbVP2IEQr7b0Rqz8LOgUMdf8",
@@ -54,5 +31,17 @@ export const agMembers = {
   "kon-aiken": {
     name: "Kon Aiken Turismo",
     placeId: "ChIJ3bqoIUQr7b0RvcpfcX5mq78",
+  },
+  "rincon-soleado": {
+    name: "Cabañas Rincón Soleado",
+    placeId: "ChIJnwhHEq4r7b0RMsCscZg_JqE",
+  },
+  "posada-del-rio": {
+    name: "Hotel Posada del Río",
+    placeId: "ChIJIR0JO0cr7b0R5QSQ9F50pzA",
+  },
+  "roca-pampa": {
+    name: "Cabalgatas Roca Pampa",
+    placeId: "ChIJf1ZkfTUr7b0RrU5bVg5DjSA",
   },
 };
